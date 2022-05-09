247e3edc09d6b30113517316bbd2122f9014eb26
Tom Rintoul <thomas.rintoul@icloud.com>
2022-05-09 16:35:55 +0100

    finished with these buttons on: normal button eraser

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+let newColour = "";
+
 function createGrid() {
     const gridContainer = document.querySelector(".grid");
     gridContainer.style.gridTemplateColumns = `repeat(16, 1fr)`;
@@ -7,11 +9,42 @@ function createGrid() {
 
     for (let i = 0; i < 16*16; i++) {
         const div = document.createElement("div");
-        // div.style.borderColor = "black";
-        // div.style.borderWidth = "thin";
         div.classList.add('grid-item');
         gridContainer.appendChild(div);
     }
 }
 
+function updateColour(colour) {
+    newColour = colour;
+    fillSquares(newColour);
+}
+
+function fillSquares(colour) {
+    console.log(squares);
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', e => {
+            e.target.style.backgroundColor = `${colour}`;
+        });
+    });
+}
+
+function clearGrid() {
+    squares.forEach((square) => square.style.backgroundColor = "white");
+}
+
+//actually running the etchasketch
+
 createGrid();
+
+const normalBtn = document.getElementById('normal');
+const eraserBtn = document.getElementById('eraser');
+const clearBtn = document.getElementById('clear');
+const squares = document.querySelectorAll('.grid-item');
+
+normalBtn.addEventListener('click', () => {
+    updateColour("black");
+});
+eraserBtn.addEventListener('click', () => {
+    updateColour("white");
+});
+clearBtn.addEventListener('click', clearGrid);
